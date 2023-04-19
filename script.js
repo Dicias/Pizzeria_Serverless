250c59c12a0f3b45aab8973ac3448fc9f7e20c0e
dicias <dmcias26@alumnos.uaq.mx>
2023-04-19 15:38:46 -0600

    Ubicacion actualizada

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ let totalPedidos;
 let totalPrecio = 0;
 
 function init(){
-
+    ubicacion();
 
     var menu_1 = document.getElementById("pizza1").onclick =function(){
     menu_1.value ="pizza_1";
@@ -19,13 +19,17 @@ function init(){
 
     var menu_2 = document.getElementById("pizza2").onclick =function(){
     menu_2.value ="pizza_2";
-    pedidoActual = menu_3.value;
+    menu_2.precio = 250;
+    pedidoActual = menu_2;
+    agregarPedido();
     //console.log(menu_2.value);
     } 
 
     var menu_3 = document.getElementById("pizza3").onclick =function(){
     menu_3.value ="pizza_3";
-    pedidoActual = menu_3.value;
+    menu_3.precio =250;
+    pedidoActual = menu_3;
+    agregarPedido();
     //console.log(menu_3.value);
     }
 
@@ -40,27 +44,36 @@ function agregarPedido(){
             
             break;
         case "pizza_2":
-            totalPedidos = pedidoActual.value;
-            totalPrecio = menu_2.precio;    
+            totalPedidos = numPedidos ++;
+            totalPrecio = menu_2.precio + totalPrecio;    
             console.log(totalPedidos, totalPrecio);
             break;
         case "pizza_3":
-            totalPedidos = pedidoActual.value;
-            totalPrecio = menu_3.precio;
+            totalPedidos = numPedidos ++;
+            totalPrecio = menu_3.precio + totalPrecio;
             console.log(totalPedidos, totalPrecio);
             break;
         }
     
+    }
 }
-}
+
+
+
 
 function ubicacion(){
-    var ubicacion = document.getElementById("ubicacion").onclick = function(){
-        console.log("a")
-
-
-    }
-
+if("geolocation" in navigator){
+    navigator.geolocation.getCurrentPosition(success, error)
 
 }
+}
 
+
+function success(position){
+    var coordenadas = position.coords;
+    console.log(coordenadas.latitude, coordenadas.longitude);
+}
+
+function error (error){
+    console.log("se trono apa");
+}
