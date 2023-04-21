595ec6d2c02ba6a0e0c761c8c05ab13841c3e305
dicias <dmcias26@alumnos.uaq.mx>
2023-04-21 07:16:14 -0600

    Modal con resumen de compra

--- script.js
+++ script.js
@@ -5,8 +5,13 @@ let totalPedidos;
 
 let totalPrecio = 0;
 
+let     pedidoPizza1 = 0;
+let     pedidoPizza2 = 0;
+let     pedidoPizza3 = 0;
+
 function init(){
     ubicacion();
+
 
     var menu_1 = document.getElementById("pizza1").onclick =function(){
     menu_1.value ="pizza_1";
@@ -31,27 +36,29 @@ function init(){
     pedidoActual = menu_3;
     agregarPedido();
     //console.log(menu_3.value);
-    }
+    //////////////
 
-    let    numPedidos = 1;
+
+}
+
 
 function agregarPedido(){
     switch(pedidoActual.value){
         case "pizza_1":
-            totalPedidos = numPedidos ++;
+            pedidoPizza1 = pedidoPizza1 + 1;
             totalPrecio = menu_1.precio + totalPrecio;
-            console.log( totalPedidos, totalPrecio);
+            console.log( "pedido1: " + pedidoPizza1, totalPrecio);
             
             break;
         case "pizza_2":
-            totalPedidos = numPedidos ++;
+            pedidoPizza2 = pedidoPizza2 + 1;
             totalPrecio = menu_2.precio + totalPrecio;    
-            console.log(totalPedidos, totalPrecio);
+            console.log("pedido2: " + pedidoPizza2, totalPrecio);
             break;
         case "pizza_3":
-            totalPedidos = numPedidos ++;
+            pedidoPizza3 = pedidoPizza3 + 1;
             totalPrecio = menu_3.precio + totalPrecio;
-            console.log(totalPedidos, totalPrecio);
+            console.log("pedido3: " + pedidoPizza3, totalPrecio);
             break;
         }
     
@@ -71,9 +78,48 @@ if("geolocation" in navigator){
 
 function success(position){
     var coordenadas = position.coords;
+    
     console.log(coordenadas.latitude, coordenadas.longitude);
 }
 
 function error (error){
     console.log("se trono apa");
+    
+}
+
+function mostrarCompra(){
+    //console.log("hola,yo te muestro la compra");
+/*
+    if(pedidoPizza1 == 0 % pedidoPizza2 == 0 & pedidoPizza3 == 0){
+        mostrar.innerHTML =" No hay pedidos aún. esta bug " + "<br>";
+    }else
+*/
+
+
+     if(pedidoPizza1 != 0){
+        mostrar.innerHTML += "Pizza 1: " + pedidoPizza1 + "<br>";
+    }
+    if(pedidoPizza2 != 0){
+        mostrar.innerHTML += "Pizza 2: " + pedidoPizza2 + "<br>";
+    }
+    if(pedidoPizza3 != 0){
+        mostrar.innerHTML += "Pizza 3: " + pedidoPizza3 + "<br>";
+    }
+    if(totalPrecio == 0){
+        mostrar.innerHTML =" No hay pedidos aún. " + "<br>";
+    }else{
+        mostrar.innerHTML += " Total: " + totalPrecio + "<br>";
+    }
+}
+
+function borrarCompra(){
+    mostrar.innerHTML = "";
+}
+
+function resetCompra(){
+    mostrar.innerHTML = "";
+    pedidoPizza1 = 0;
+    pedidoPizza2 = 0;
+    pedidoPizza3 = 0;
+    totalPrecio = 0;
 }
