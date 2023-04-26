e4d1b11897565c1c0fff97a291d48eba3216e090
dicias <dmcias26@alumnos.uaq.mx>
2023-04-25 21:26:49 -0600

    falta enviar los pedidos

--- script.js
+++ script.js
@@ -8,10 +8,9 @@ let totalPrecio = 0;
 let     pedidoPizza1 = 0;
 let     pedidoPizza2 = 0;
 let     pedidoPizza3 = 0;
-
+let     coordsGlobales;
 
 function init(){
-    enviarUbicacion();
     ubicacion();
 
 
@@ -41,6 +40,7 @@ function init(){
     //////////////
 
 }
+
 
 //Guardar pedidos en variables
 function agregarPedido(){
@@ -76,14 +76,27 @@ if("geolocation" in navigator){
 function success(position){
     var coordenadas = position.coords;
     console.log(coordenadas.latitude, coordenadas.longitude);
-
+    var cords = coordenadas
     initMap(coordenadas);
+    coordsGlobales = coordenadas;
 }
 
 function error (error){
     alert("se trono apa");
     
 }
+
+/*
+function mostrarCoors(){
+
+    var latitude = coordsGlobales.latitude;
+    var longitude = coordsGlobales.longitude;
+    console.log(latitude, longitude + "ya funka");
+
+
+}
+*/
+
 
 //Creación el mapa
 function initMap(coordenadas) {
@@ -95,7 +108,7 @@ function initMap(coordenadas) {
     }).addTo(map);
     var marker = L.marker([coordenadas.latitude, coordenadas.longitude]).addTo(map);
     marker.bindPopup("<b>Aquí van a caer</b><br>tus pizzas.").openPopup();
-
+    
 }
 
 function mostrarCompra(){
@@ -129,30 +142,29 @@ function resetCompra(){
     totalPrecio = 0;
 }
 
-/*
-const enviarUbicacion = (coordenadas) => {
-    fetch('https://sgfsg3d586.execute-api.us-east-2.amazonaws.com/default/funcionUbicacion', {
-      method: 'POST',
-      body: JSON.stringify({coordenadas: coordenadas})
-    })
-    .then(response => response.json())
-    .then(data => console.log(data))
-    .catch(error => console.error(error));
-  };
-*/
 
-  const enviarUbicacion = async () => {
-    const ubi = {
-      latitud: 37.7749,
-      longitud: -122.4194
-    };
+
+const enviarUbicacionLambda = async () => {
+    let latitude = coordsGlobales.latitude
+    let longitude = coordsGlobales.longitude
+    const lambda =`https://900d8x4r1a.execute-api.us-east-2.amazonaws.com/test/testcors?latitude=${latitude}&longitude=${longitude}`
     
-    const response = await fetch('https://sgfsg3d586.execute-api.us-east-2.amazonaws.com/default/funcionUbicacion', {
-      method: 'POST',
-      body: JSON.stringify({ ubi }),
-    });
-    
-    const data = await response.json();
-    alert(data);
-    console.log("lo logro señor");
-  };
+    await fetch(lambda, {
+        method: 'GET',
+        mode: 'cors',
+        headers: {
+          'Content-Type': 'application/json'
+  
+        }
+      })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Error al enviar datos');
+        }
+        alert(`Datos enviados con éxito! Estas son las coordenadas: ` + latitude +" " + longitude);
+        alert("Falta enviar los pedidos enviados tambien")
+        return response.json();
+      })
+      .catch(error => console.error('Error:', error));
+    }
+
